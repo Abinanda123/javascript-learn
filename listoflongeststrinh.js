5404070ecf00d76c0d9fbbf00202620993408e31
Takhellambam Abinanda Singh <seityabari2017@gmail.com>
2026-02-15 19:54:12 +0530

    hi

---
+++ listoflongeststrinh.js
@@ -0,0 +1,15 @@
+const lenghtoflongeststring = (s) =>{
+    let set = new Set();
+    let max = 0;
+    let left = 0;
+    for(let right = 0; right < s.length; right++){
+        while(set.has(s[right])){
+            set.delete(s[left]);
+            left++;
+        }
+        set.add(s[right]);
+        max = Math.max(max, right - left + 1);
+    }
+    return max;
+}
+console.log(lenghtoflongeststring("abcabcbb")); // Output: 3
